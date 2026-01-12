19f6e6816d9e72b86bc5f84205e47e83e23ca583
hhjo <43205471+hhjo96@users.noreply.github.com>
2026-01-13 08:59:45 +0900

    예외처리 완료

--- src/main/java/com/example/standardpractice1/exception/MemberCountOver10Exception.java
+++ src/main/java/com/example/standardpractice1/exception/MemberCountOver10Exception.java
@@ -6,6 +6,6 @@ import org.springframework.http.HttpStatus;
 public class MemberCountOver10Exception extends ServiceException {
 
     public MemberCountOver10Exception(String message) {
-        super(HttpStatus.BAD_REQUEST, message);
+        super(ErrorCode.USER_LIMIT_EXCEEDED, message, HttpStatus.BAD_REQUEST);
     }
 }
